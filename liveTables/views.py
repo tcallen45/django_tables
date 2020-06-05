a9937bbd943d3458029b77f334d50b41d80b2b6a
unknown <tpc3yw@virginia.edu>
2020-06-05 16:45:17 -0400

    changes for heroku

--- liveTables/views.py
+++ liveTables/views.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import generic
@@ -6,15 +5,11 @@ from django.db.models import Q
 from django.core.exceptions import ValidationError
 from .models import Match, Team
 from django.template import loader
-from .tasks import myTask
 import http.client
 import json
 
 connection = http.client.HTTPConnection('api.football-data.org')
 headers = { 'X-Auth-Token': '4e89e43e91b341d6a13e9d2b7a7c939d' }
-
-# myTask()
-
 
 
 def get_matches():
@@ -22,7 +17,6 @@ def get_matches():
     response = json.loads(connection.getresponse().read().decode())
     matches = response["matches"]
     for match in matches:
-        print(match)
         homeTeam = match["homeTeam"]["name"]
         homeScore = match["score"]["fullTime"]["homeTeam"]
         awayTeam = match["awayTeam"]["name"]
@@ -37,40 +31,7 @@ def get_matches():
         m.status = match_status
         if(m.status == 'FINISHED'):
             m.setResult()
-        m.save()
-        # if(match_status == "SCHEDULED"):
-        #     m, created = Match.objects.get_or_create(
-        #         home_team = homeTeam,
-        #         away_team = awayTeam,
-        #         match_number = matchID,
-        #         status = match_status
-        #     )
-        # elif(match_status == "IN-PLAY" or match_status == "PAUSED" or match_status == "LIVE"):
-        #     m, created = Match.objects.get_or_create(match_number = matchID)
-        #     # if created:
-        #     m.home_team = homeTeam
-        #     m.home_score = match["score"]["fullTime"]["homeTeam"],
-        #     m.away_team = awayTeam,
-        #     m.away_score = match["score"]["fullTime"]["awayTeam"],
-        #     m.status = match_status
-        #     # else:
-
-        #     # m.setResult()
-        #     # m.save()
-        #     # print(m.result) 
-
-        # elif(match_status == "FINISHED"):
-        #     m, created = Match.objects.get_or_create(
-        #         home_team = homeTeam,
-        #         home_score = match["score"]["fullTime"]["homeTeam"],
-        #         away_team = awayTeam,
-        #         away_score = match["score"]["fullTime"]["awayTeam"],
-        #         match_number = matchID,
-        #         status = match_status
-        #     )
-        #     m.setResult()
-        #     m.save()         
-
+        m.save()        
 
 # def get_teams():
 #     connection.request('GET', '/v2/competitions/BL1/matches', None, headers )
@@ -110,7 +71,3 @@ def schedule(request):
         'matches': matches,
     }
     return HttpResponse(template.render(context, request))
-
-
-
-# Create your views here.
