fa2c484b55cd879b8de7ebaddba9c84b43e95e8f
unknown <tpc3yw@virginia.edu>
2020-06-05 16:49:08 -0400

    settings.py changes for heroku

--- SoccerTables/settings.py
+++ SoccerTables/settings.py
@@ -133,3 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    # Configure Django App for Heroku.
+    import django_heroku
+    django_heroku.settings(locals())
+except ImportError:
+    found = False
